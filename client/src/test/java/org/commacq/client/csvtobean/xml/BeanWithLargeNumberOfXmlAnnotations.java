a1f179a3ae60ce8d714d1379006b43575512ab67
daveboden <dave@daveboden.com>
2014-02-10 08:40:36 +0000

    Add performance test.

---
+++ client/src/test/java/org/commacq/client/csvtobean/xml/BeanWithLargeNumberOfXmlAnnotations.java
@@ -0,0 +1,138 @@
+package org.commacq.client.csvtobean.xml;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class BeanWithLargeNumberOfXmlAnnotations {
+
+	@XmlAttribute
+	String id;
+	
+	@XmlAttribute
+	String name0;
+	@XmlAttribute
+	String name1;
+	@XmlAttribute
+	String name2;
+	@XmlAttribute
+	String name3;
+	@XmlAttribute
+	String name4;
+	@XmlAttribute
+	String name5;
+	@XmlAttribute
+	String name6;
+	@XmlAttribute
+	String name7;
+	@XmlAttribute
+	String name8;
+	@XmlAttribute
+	String name9;
+	@XmlAttribute
+	String name10;
+	@XmlAttribute
+	String name11;
+	@XmlAttribute
+	String name12;
+	@XmlAttribute
+	String name13;
+	@XmlAttribute
+	String name14;
+	@XmlAttribute
+	String name15;
+	@XmlAttribute
+	String name16;
+	@XmlAttribute
+	String name17;
+	@XmlAttribute
+	String name18;
+	@XmlAttribute
+	String name19;
+
+	
+	public String getId() {
+		return id;
+	}
+	
+	public String getName0() {
+		return name0;
+	}
+
+	public String getName1() {
+		return name1;
+	}
+
+	public String getName2() {
+		return name2;
+	}
+
+	public String getName3() {
+		return name3;
+	}
+
+	public String getName4() {
+		return name4;
+	}
+
+	public String getName5() {
+		return name5;
+	}
+
+	public String getName6() {
+		return name6;
+	}
+
+	public String getName7() {
+		return name7;
+	}
+
+	public String getName8() {
+		return name8;
+	}
+
+	public String getName9() {
+		return name9;
+	}
+
+	public String getName10() {
+		return name10;
+	}
+
+	public String getName11() {
+		return name11;
+	}
+
+	public String getName12() {
+		return name12;
+	}
+
+	public String getName13() {
+		return name13;
+	}
+
+	public String getName14() {
+		return name14;
+	}
+
+	public String getName15() {
+		return name15;
+	}
+
+	public String getName16() {
+		return name16;
+	}
+
+	public String getName17() {
+		return name17;
+	}
+
+	public String getName18() {
+		return name18;
+	}
+
+	public String getName19() {
+		return name19;
+	}
+	
+}
